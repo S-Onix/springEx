47976eb33c745d1184d04ee195883d9d5ce9fb21
S-Onix <msyoo101@hansung.ac.kr>
2019-01-02 15:08:23 +0900

    inner xml file

---
+++ springapp/src/ex4/Main.java
@@ -0,0 +1,12 @@
+package ex4;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		Greeter g = ctx.getBean("greeter", Greeter.class);
+		String msg = g.greet("½ºÇÁ¸µ");
+		System.out.println(msg);
+	}
+}
