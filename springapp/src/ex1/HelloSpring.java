a159a1be05b94e848cf1a4ae2e705852168c1d63
S-Onix <msyoo101@hansung.ac.kr>
2019-01-02 14:07:31 +0900

    기본 객체 생성

---
+++ springapp/src/ex1/HelloSpring.java
@@ -0,0 +1,8 @@
+package ex1;
+
+public class HelloSpring {
+	public static void main(String[] args) {
+		MessageBean bean = new MessageBean();
+		bean.sayHello("spring");
+	}
+}
---
+++ springapp/src/ex1/MessageBean.java
@@ -0,0 +1,7 @@
+package ex1;
+
+public class MessageBean {
+	public void sayHello(String msg) {
+		System.out.println("Hello, " + msg);
+	}
+}
