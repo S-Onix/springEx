dda44e02bc3aac167c4e3d15d87f9863d4de5691
S-Onix <msyoo101@hansung.ac.kr>
2019-01-03 17:17:37 +0900

    delete Map datastructure

--- springminiproject/src/dao/MemberDao.java
+++ springminiproject/src/dao/MemberDao.java
@@ -6,15 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import db.DBAction;
 import vo.Member;
 
 public class MemberDao {
-	private static long nextId = 0;
-	private Map<String, Member> map = new HashMap<>();
 	
 	public Member selectByEmail(String email) {
 		Connection conn = null;
@@ -75,7 +71,6 @@ public class MemberDao {
 	}
 	
 	public void update(Member member) {
-//		map.put(member.getEmail(), member);
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		
